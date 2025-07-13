6df3e21883c60417be6d2fda2e17e3b42f3eea97
Karn <ksandh24@students.cs.ubc.ca>
2025-07-13 14:51:34 -0700

    Added specifications for WorkoutTrackerApp ui class

---
+++ src/main/ui/WorkoutTrackerApp.java
@@ -0,0 +1,44 @@
+package ui;
+
+import model.WorkoutLog;
+
+import java.util.Scanner;
+
+public class WorkoutTrackerApp {
+    
+    private WorkoutLog log;
+    private Scanner input;
+
+    //EFFECTS: Runs the app
+    public WorkoutTrackerApp() {
+        //
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Processes user input
+    private void runWorkoutTracker() {
+        //
+    }
+
+    //EFFECTS: displays a menu of options to the user
+    private void displayMenu() {
+        //
+    }
+
+    //EFFECTS: asks user for workout date and then asks user for exercise(s) info
+    //         then creates a workout session to add to workout log
+    private void addWorkoutSession() {
+        //
+    }
+
+    //EFFECTS: showcases all workout sessions that have been added to workout log
+    private void viewWorkoutSessions() {
+        //
+    }
+
+    //EFFECTS: displays personal record for given exercise
+    private void viewPersonalRecord() {
+        //
+    }
+
+}
