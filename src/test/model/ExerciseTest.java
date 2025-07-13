09ae93565792bae548158e10c1aded3497ee0ce7
Karn <ksandh24@students.cs.ubc.ca>
2025-07-12 20:30:38 -0700

    Added tests for Exercise class

---
+++ src/test/model/ExerciseTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExerciseTest {
+
+    private Exercise exercise;
+    
+    @BeforeEach
+    void runBefore() {
+        exercise = new Exercise("Bench Press", 10, 3, 135);
+    }
+
+    @Test
+    void testConductor() {
+        assertEquals("Bench Press", exercise.getName());
+        assertEquals(10, exercise.getReps());
+        assertEquals(3, exercise.getSets());
+        assertEquals(135, exercise.getWeight());
+    }
+}
