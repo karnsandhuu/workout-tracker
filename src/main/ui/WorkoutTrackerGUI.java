7cbf6792cd63c82ce70ded32b8a35e63fec923c2
Karn <ksandh24@students.cs.ubc.ca>
2025-07-26 00:02:47 -0700

    Added method to load a tab

--- src/main/ui/WorkoutTrackerGUI.java
+++ src/main/ui/WorkoutTrackerGUI.java
@@ -5,9 +5,11 @@ import javax.swing.*;
 
 
 public class WorkoutTrackerGUI extends JFrame {
-
+    public static final int ADD_TAB_INDEX = 0;
     public static final int WIDTH = 600;
     public static final int HEIGHT = 500;
+
+    private JTabbedPane sidebar;
 
     public static void main(String[] args) {
         new WorkoutTrackerGUI();
@@ -17,7 +19,17 @@ public class WorkoutTrackerGUI extends JFrame {
         setTitle("Workout Tracker");
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        sidebar = new JTabbedPane();
+        sidebar.setTabPlacement(JTabbedPane.LEFT);
+        loadTabs();
+        add(sidebar);
 
         setVisible(true);
     }
+
+    private void loadTabs() {
+        JPanel addWorkoutTab = new JPanel(); 
+        sidebar.add(addWorkoutTab, ADD_TAB_INDEX);
+        sidebar.setTitleAt(ADD_TAB_INDEX, "Add Workout");
+}
 }
